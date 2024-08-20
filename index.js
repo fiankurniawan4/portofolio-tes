4dc658fbeb6a31228eafa69ed3a0c4cc22e264fa
fiankurniawan4 <anfian284@gmail.com>
2024-08-20 19:01:14 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
