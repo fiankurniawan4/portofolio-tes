32095bf6b86d61326a730a48faff0c111aad1f86
fiankurniawan4 <anfian284@gmail.com>
2024-10-25 18:42:35 +0700

    up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
